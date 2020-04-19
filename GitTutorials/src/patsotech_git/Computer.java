8f1357540e211e3af62fab321c4b6e09068aab1d
patrickpatso <patrickpatso95@gmail.com>
2020-04-20 07:25:43 +0800

    This is my first commit

---
+++ GitTutorials/src/patsotech_git/Computer.java
@@ -0,0 +1,28 @@
+package patsotech_git;
+
+public class Computer {
+	int id;
+	String name;
+	String cversion;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCversion() {
+		return cversion;
+	}
+	public void setCversion(String cversion) {
+		this.cversion = cversion;
+	}
+	
+	
+
+}
---
+++ GitTutorials/src/patsotech_git/MainComputer.java
@@ -0,0 +1,12 @@
+package patsotech_git;
+
+public class MainComputer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("the computer name is ");
+
+	}
+
+}
